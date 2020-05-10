4b01e60be0ef440d1e1214e6384e0451dd68eb19
ethan-sun-010 <suny103@mcmaster.ca>
2020-05-10 19:40:07 -0400

    update colorscale

--- assignment_2/Question_1.py
+++ assignment_2/Question_1.py
@@ -2,32 +2,32 @@
 import numpy
 import math
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
-
 
 c_converge_real = []
 c_converge_imag = []
 c_diverge_real = []
 c_diverge_imag = []
+diverge_iteration_number = []
 
-for x in numpy.arange(-2.0, 2.0, 0.01):
-    for y in numpy.arange(-2.0, 2.0, 0.01):
+for x in numpy.arange(-2.0, 2.0, 0.05):
+    for y in numpy.arange(-2.0, 2.0, 0.05):
         c = x + y*1j
         z = 0
-        for i in range(100):
+        a = 1
+        for i in range(25):
             z = z**2 + c
+            if numpy.isfinite(z.real) and numpy.isfinite(z.imag):
+                a = a + 1
         if numpy.isfinite(z.real) and numpy.isfinite(z.imag):
             c_converge_real.append(c.real)
             c_converge_imag.append(c.imag)
         else:
             c_diverge_real.append(c.real)
             c_diverge_imag.append(c.imag)
+            diverge_iteration_number.append(a)
 
-plt.scatter(c_converge_real,c_converge_imag, label = 'convergent c points', color='red')
-plt.scatter(c_diverge_real,c_diverge_imag, label = 'divergent c points', color='blue')
-plt.xlabel('x')
-plt.ylabel('y')
+conv = plt.scatter(c_converge_real,c_converge_imag, s = 10, c = "red", label = "convergent points")
+div = plt.scatter(c_diverge_real,c_diverge_imag, s = 10, c=diverge_iteration_number, label = "divergent points")
+plt.colorbar()
 plt.legend()
 plt.show()
-
-
